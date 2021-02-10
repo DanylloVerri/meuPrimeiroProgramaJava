9becdb2f455eb9165f323a0da7f87b0a62df8883
Danyllo Verri <danylloverri@gmail.com>
2021-02-10 19:29:24 -0300

    Add files via upload
    Primeira versão do primeiro projeto em java

---
+++ firstJavaProject/src/firstJavaProject/Main.java
@@ -0,0 +1,34 @@
+/**
+ * Meu primeiro programa Java
+ * 
+ * Este programa mostra ao aluno que:
+ * 1) Um programa Java é construído dentro de um projeto Java.
+ * 2) Todo programa Java deve ter uma classe com um método main.
+ * 3) Como exibir informações no terminal (console) do Java.
+ * 4) Como ler uma string do terminal (console) do Java.
+ * 
+ */
+package firstJavaProject;
+
+import java.util.Scanner;
+
+/**
+ * @author danyl
+ *
+ */
+public class Main { // Classe que contém o método principal.
+	public static void main(String[] args) { // Este é o método principal.
+		
+		System.out.println("Olá amigo! \nQual é o seu nome?");
+		
+		// Ler uma string do console usando a biblioteca Scanner
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		String nome = in.nextLine();
+		
+		// Exibir uma string usando printf.
+		System.out.printf("Olá %s!", nome);
+		
+	}
+
+}
